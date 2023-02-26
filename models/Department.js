ef3548475201703efe65544b3e12ff69c453b32e
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-02-26 15:19:23 +1100

    Additional basic layout

---
+++ models/Department.js
@@ -0,0 +1,6 @@
+/* `department`
+
+    * `id`: `INT PRIMARY KEY`
+
+    * `name`: `VARCHAR(30)` to hold department name
+*/
---
+++ models/Employee.js
@@ -0,0 +1,12 @@
+/* `employee`
+
+    * `id`: `INT PRIMARY KEY`
+
+    * `first_name`: `VARCHAR(30)` to hold employee first name
+
+    * `last_name`: `VARCHAR(30)` to hold employee last name
+
+    * `role_id`: `INT` to hold reference to employee role
+
+    * `manager_id`: `INT` to hold reference to another employee that is the manager of the current employee (`null` if the employee has no manager)
+*/
