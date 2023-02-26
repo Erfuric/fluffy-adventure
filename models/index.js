02990961284a3546f8226dd0aec855fcf52e3303
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-02-26 16:07:08 +1100

    model index update

--- models/index.js
+++ models/index.js
@@ -2,22 +2,48 @@ const Employee = require('./Employee');
 const Department = require('./Department');
 const Role = require('./Role');
 
-Reader.hasOne(LibraryCard, {
-  foreignKey: 'reader_id',
-  onDelete: 'CASCADE',
+Employee.hasOne(Role, {
+    foreignKey: 'employee_id',
+    onDelete: 'CASCADE',
 });
 
-Reader.hasMany(Book, {
-  foreignKey: 'reader_id',
-  onDelete: 'CASCADE',
+Employee.hasOne(Department, {
+    foreignKey: 'employee_id',
+    onDelete: 'CASCADE',
 });
 
-Book.belongsTo(Reader, {
-  foreignKey: 'reader_id',
+Department.hasMany(Roles, {
+    // foreignKey: 'employee_id',
 });
 
-LibraryCard.belongsTo(Reader, {
-  foreignKey: 'reader_id',
+Department.hasMany(Employee, {
+    foreignKey: 'employee_id',
 });
 
-module.exports = { Reader, Book, LibraryCard };
+Role.hasMany(Employee, {
+    foreignKey: 'employee_id',
+});
+
+Role.hasMany(Department, {
+    // foreignKey: 'employee_id',
+});
+
+module.exports = { Department, Employee, Role };
+
+/*
+    One to one, one to many, many to many
+
+    Departments can have multiple roles and employees
+        Department hasmany roles
+        Department hasmany employees
+
+    Roles can have multiple employees and departments
+        Role hasmany employees
+        Role hasmany departments
+
+    Employees can have one role and department
+        Employee hasone role
+        Employee hasone department
+
+
+*/
